f8667fbddfcb688f4a13ea4327e5434c4e51f096
jiang <jiang@com>
2020-05-20 13:34:15 +0800

    修改本地工程

--- git_project/src/main/java/test1.java
+++ git_project/src/main/java/test1.java
@@ -2,4 +2,8 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("hello !");
     }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
 }
